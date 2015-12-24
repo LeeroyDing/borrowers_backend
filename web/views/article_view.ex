96d3ebfca778fbae61b7d6f634b6d5ed45ebb66a
Leeroy Ding <i@leeroy.im>
2015-12-24 16:48:07 +0000

    ja_serializer relationships

--- web/views/article_view.ex
+++ web/views/article_view.ex
@@ -1,6 +1,8 @@
 defmodule BorrowersBackend.ArticleView do
   use BorrowersBackend.Web, :view
 
-  attributes [:created_at, :description, :notes, :state, :friend]
+  attributes [:created_at, :description, :notes, :state]
+
+  has_one :friend
 
 end
--- web/views/friend_view.ex
+++ web/views/friend_view.ex
@@ -1,5 +1,8 @@
 defmodule BorrowersBackend.FriendView do
   use BorrowersBackend.Web, :view
 
-  attributes [:first_name, :last_name, :email, :twitter, :total_articles]
+  attributes [:first_name, :last_name, :email, :twitter]
+  
+  has_many :articles
+  
 end
